7e9de4f0b3cd81e706901beaac09505a0370ef46
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2016-05-27 05:58:09 -0500

    msm: camera: Check for array index out of bounds Add checks in msm_mctl_pp_reserve_free/release_frame to prevent index out of bounds.
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/media/video/msm/msm_mctl_pp.c
+++ drivers/media/video/msm/msm_mctl_pp.c
@@ -517,7 +517,7 @@ int msm_mctl_pp_reserve_free_frame(
 	}
 
 	image_mode = div_frame.image_mode;
-	if (image_mode <= 0) {
+	if (image_mode < 0  || image_mode >= MSM_MAX_IMG_MODE) {
 		pr_err("%s Invalid image mode %d", __func__, image_mode);
 		return -EINVAL;
 	}
@@ -570,7 +570,7 @@ int msm_mctl_pp_release_free_frame(
 	}
 
 	image_mode = div_frame.image_mode;
-	if (image_mode < 0) {
+	if (image_mode < 0 || image_mode >= MSM_MAX_IMG_MODE) {
 		pr_err("%s Invalid image mode %d\n", __func__, image_mode);
 		return -EINVAL;
 	}
