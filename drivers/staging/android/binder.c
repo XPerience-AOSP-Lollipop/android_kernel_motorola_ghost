7bd9b1b0ef5e2c012df25fee37a2564e3da3465b
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-18 05:36:57 -0500

    Revert "staging: android: binder: Debugging FD leak"
    This reverts commit b532fa8e5da298d5a310dbfa65ae6c3ca8baa801.

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -35,7 +35,6 @@
 #include <linux/vmalloc.h>
 #include <linux/slab.h>
 #include <linux/security.h>
-#include <linux/fdleak_dbg.h>
 
 #include "binder.h"
 
@@ -449,7 +448,6 @@ static void task_fd_install(
 	BUG_ON(fdt->fd[fd] != NULL);
 	rcu_assign_pointer(fdt->fd[fd], file);
 	spin_unlock(&files->file_lock);
-	warn_if_big_fd(fd, proc->tsk);
 }
 
 /*
